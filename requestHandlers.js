41acc4b9eba8fee046d60a4db9aa94fe1c10c4b2
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-14 23:35:56 -0700

    added image show

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,41 @@
+const querystring = require('querystring');
+const fs =require('fs');
 
-function start(){
+function start(res, postData){
     console.log('Request handler "start" was called.');
+
+    var body = '<html>' +
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(body);
+    res.end();  
 }
 
-function upload() {
+function upload(res, postData) {
     console.log('Request handler "upload" was called.');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('You have sent the text: '+ querystring.parse(postData).text);
+    res.end();
+}
+
+function show(res){
+    console.log('Request handler "show" was called.');
+    res.writeHead(200, { 'Content-Type': 'image/png'});
+    fs.createReadStream('./tmp/test.png').pipe(res);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- router.js
+++ router.js
@@ -1,12 +1,14 @@
-// const requestHandlers = require('./requestHandlers');
 
 
-function route(handle, pathname){
+function route(handle, pathname, res, postData){
     console.log('About to route a request for' + pathname);
     if(typeof handle[pathname] === 'function'){
-        handle[pathname]();
+        handle[pathname](res, postData);
     }else {
         console.log('No request handler found for '+ pathname);
+        res.writeHead(404,{'Content-Type': 'text/plain'});
+        res.write('404 Not Found');
+        res.end();
     }
 }
 
