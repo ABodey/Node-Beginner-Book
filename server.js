5bd5d2306e043fb61041ccd94ba37f21b31c0bf7
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-14 20:05:52 -0700

    added lint and request handlers

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const http = require('http');
+const url = require('url');
 
-function start() {
-function onRequest(req, res){
-    console.log('Request received.');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
-}
+function start(route, handle) {
+    function onRequest(req, res){
+        const pathname = url.parse(req.url).pathname;
+        console.log('Request for'+ pathname +'received.');
 
-http.createServer(onRequest).listen(8888);
+        route(handle, pathname);
 
-console.log('Server has started.');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Hello World');
+        res.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
 }
 
 exports.start = start;
